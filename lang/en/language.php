f1987a457af6b3a99ead84423c81a46644274af0
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-07 18:36:42 +0200

    refactor lang

---
+++ lang/en/language.php
@@ -0,0 +1,22 @@
+<?php
+
+$LANG = array();
+
+// Translation of file modules/menus/module.php
+
+$LANG['Sort menu'] = 'Sort menu';
+$LANG['Saving item ... '] = 'Saving item ... ';
+$LANG['Click to edit'] = 'Click to edit';
+$LANG['Click on the menu items and edit them. Empty will delete'] = 'Click on the menu items and edit them. Empty will delete';
+$LANG['Menu has been reset'] = 'Menu has been reset';
+$LANG['Confirm reset'] = 'Confirm reset';
+$LANG['Menu item deleted'] = 'Menu item deleted';
+$LANG['Pick menu section'] = 'Pick menu section';
+// Translation of file modules/menus/install.inc
+
+$LANG['Menus'] = 'Menus';
+// Translation of file modules/menus/menu.inc
+
+$LANG['Sort menu'] = 'Sort menu';
+$LANG['Edit titles'] = 'Edit titles';
+$LANG['Reset menus'] = 'Reset menus';
