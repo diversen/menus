16cfedd32513df7a5c6dbb377a51c7f249499f22
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-08 15:19:06 +0200

    lang

---
+++ lang/da/language.php
@@ -0,0 +1,15 @@
+<?php
+
+$LANG = array();
+
+$LANG['Sort menu'] = 'Sort menu';
+$LANG['Saving item ... '] = 'Gemmer element ...';
+$LANG['Click to edit'] = 'Klik for at redigere';
+$LANG['Click on the menu items and edit them. Empty will delete'] = 'Klik på menupunkterne og redigere dem. Tøm sletter';
+$LANG['Menu has been reset'] = 'Menuen er blevet nulstillet';
+$LANG['Confirm reset'] = 'Bekræft reset';
+$LANG['Menu item deleted'] = 'Menupunkt slettet';
+$LANG['Pick menu section'] = 'Pick-menu sektion';
+$LANG['Menus'] = 'Menuer';
+$LANG['Edit titles'] = 'Rediger titler';
+$LANG['Reset menus'] = 'Nulstil menuer';
